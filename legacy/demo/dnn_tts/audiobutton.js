a21c11f46ebf79150b761541641667622a826aa7
Zhizheng Wu <zhizhengwu@zhizhengs-imac.lan>
2022-02-19 22:29:11 -0800

    new branch using academic pages

---
+++ legacy/demo/dnn_tts/audiobutton.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    var myAudio = document.createElement('audio');
+    myAudio.onended = function () {
+        $('a.audio').each(function (index, item) {
+            $(item).html('<img class="imgButton" src="/Content/play.png" />');
+        });
+    };
+    if (!myAudio.canPlayType('audio/wav')) {
+        // Replace by download text
+        $('audio').each(function (index, item) {
+            $(item).replaceWith('<a href="' + item.src + '" target="_blank"><img class="imgButton" src="/Content/play.png" /></a>');
+        });
+    } else {
+        // Add by play button
+        $('audio').each(function (index, item) {
+            $(item).replaceWith('<a class="audio" href="' + item.src + '" target="_blank"><img class="imgButton" src="/Content/play.png" /></a>');
+        });
+
+        $('a.audio').click(function (e) {
+            e.preventDefault();
+
+            var tag = eval('this');
+            var jtag = $(tag);
+            console.log(jtag.html());
+            if (jtag.html().indexOf("play.png") >= 0) {
+                $('a.audio').each(function (index, item) {
+                    $(item).html('<img class="imgButton" src="/Content/play.png" />');
+                });
+                jtag.html('<img class="imgButton" src="/Content/stop.png" />');
+
+                myAudio.src = tag.href;
+                myAudio.play();
+            } else {
+                jtag.html('<img class="imgButton" src="/Content/play.png" />');
+                myAudio.pause();
+            }
+        });
+    }
+});
+//# sourceMappingURL=audiobutton.js.map
